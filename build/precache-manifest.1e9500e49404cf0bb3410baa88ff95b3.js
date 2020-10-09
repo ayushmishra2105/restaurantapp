0c5718b26ed848f6e170bdf1cafa6e33e948559b
ayushmishra2105 <68776955+ayushmishra2105@users.noreply.github.com>
2020-10-09 22:29:31 +0530

    react production build

---
+++ build/precache-manifest.1e9500e49404cf0bb3410baa88ff95b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c4a6081da0bc4498105b1be59b4d5f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b755f47be261c5726dd",
+    "url": "/static/css/main.40a70472.chunk.css"
+  },
+  {
+    "revision": "5ab355d898a4aa556203",
+    "url": "/static/js/2.95677ba0.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/static/js/2.95677ba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b755f47be261c5726dd",
+    "url": "/static/js/main.c54032e1.chunk.js"
+  },
+  {
+    "revision": "76e1647710602da6d3db",
+    "url": "/static/js/runtime-main.01240366.js"
+  }
+]);
